1d78b3e700aa716a562fc636ceb32f07e0faae8e
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-07-05 01:26:48 +0800

    update UI

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -45,52 +45,145 @@ class _LandingPageState extends State<Landingpage> {
               }),
           displayMode: PaneDisplayMode.compact,
           items: [
-            PaneItem(icon: Icon(FluentIcons.more), title: Text("data")),
-            PaneItem(icon: Icon(FluentIcons.help), title: Text('hey there')),
+            PaneItem(
+                icon: Icon(FluentIcons.folder), title: Text("School Data")),
+            PaneItem(icon: Icon(FluentIcons.settings), title: Text('Settings')),
           ]),
       content: NavigationBody(
         index: index,
         children: [
-          ScaffoldPage(
-            header: Text('Schools'),
-            content: Row(
-              children: [
-                Column(
-                  children: [
-                    Row(
+          Padding(
+            padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+            child: ScaffoldPage(
+              header: Text('Grading System'),
+              content: Row(
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height - 50,
+                        alignment: Alignment.topLeft,
+                        padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 219, 218, 218),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Row(
+                          children: [
+                            Text('Schools'),
+                            Button(
+                                child: Text('Add School'),
+                                onPressed: () {
+                                  index++;
+                                  ContentDialog(
+                                    title: Text('Add School'),
+                                    content: TextBox(
+                                      controller: null,
+                                      header: 'School Name',
+                                      placeholder: 'School name',
+                                    ),
+                                    backgroundDismiss: true,
+                                    actions: [
+                                      Button(
+                                          child: Text('add Schoool'),
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          })
+                                    ],
+                                  );
+                                })
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+                    child: Column(
                       children: [
-                        Text('column 1'),
-                        Button(
-                            child: Text('Add School'),
-                            onPressed: () {
-                              index++;
-                              ContentDialog(
-                                title: Text('Add School'),
-                                content: TextBox(
-                                  controller: null,
-                                  header: 'School Name',
-                                  placeholder: 'School name',
-                                ),
-                                backgroundDismiss: true,
-                                actions: [
-                                  Button(
-                                      child: Text('add Schoool'),
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      })
-                                ],
-                              );
-                            })
+                        Container(
+                          height: MediaQuery.of(context).size.height - 50,
+                          alignment: Alignment.topLeft,
+                          padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 219, 218, 218),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Row(
+                            children: [
+                              Text('Subjects'),
+                              Button(
+                                  child: Text('Add Subject'),
+                                  onPressed: () {
+                                    index++;
+                                    ContentDialog(
+                                      title: Text('Add Subject'),
+                                      content: TextBox(
+                                        controller: null,
+                                        header: 'Subject Name',
+                                        placeholder: 'Subject name',
+                                      ),
+                                      backgroundDismiss: true,
+                                      actions: [
+                                        Button(
+                                            child: Text('add Subject'),
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            })
+                                      ],
+                                    );
+                                  })
+                            ],
+                          ),
+                        ),
                       ],
                     ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Text('hello'),
-                  ],
-                )
-              ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+                    child: Column(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width - 400,
+                          height: MediaQuery.of(context).size.height - 50,
+                          alignment: Alignment.topLeft,
+                          padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 219, 218, 218),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Row(
+                            children: [
+                              Text('Students'),
+                              Button(
+                                  child: Text('Add Students'),
+                                  onPressed: () {
+                                    index++;
+                                    ContentDialog(
+                                      title: Text('Add Students'),
+                                      content: TextBox(
+                                        controller: null,
+                                        header: 'Students Name',
+                                        placeholder: 'Students name',
+                                      ),
+                                      backgroundDismiss: true,
+                                      actions: [
+                                        Button(
+                                            child: Text('add Students'),
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            })
+                                      ],
+                                    );
+                                  })
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
           ScaffoldPage(
